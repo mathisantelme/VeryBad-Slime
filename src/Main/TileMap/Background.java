28b6c75518491f1840dccbf13335689f7d0f8112
Mathis <mathis.antelme@gmail.com>
2018-03-08 13:57:05 +0100

    Création de la classe qui charge le background

---
+++ src/Main/TileMap/Background.java
@@ -0,0 +1,48 @@
+package Main.TileMap;
+
+import Main.Game;
+import Main.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Background {
+
+    private BufferedImage image;
+
+    private double posX, posY;
+    private double dx, dy;
+
+    private double moveScale;
+
+    public Background (String path, double ms) {
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream(path));
+            moveScale = ms;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setPosition (double x, double y) {
+        this.posX = (x * moveScale) % GamePanel.WIDTH;
+        this.posY = (y * moveScale) % GamePanel.HEIGHT;
+    }
+
+    public void setVector (double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void update () {
+        this.posX += this.dx;
+        this.posY += this.dy;
+    }
+
+    public void draw (Graphics2D g) {
+        g.drawImage(image, (int) posX, (int) posY, null);
+        if (posX < 0) g.drawImage(image, (int) posX + GamePanel.WIDTH, (int) posY, null);
+        if (posX > 0) g.drawImage(image, (int) posX - GamePanel.WIDTH, (int) posY, null);
+    }
+}
